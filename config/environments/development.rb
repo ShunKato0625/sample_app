91b5bc9f0f7d0984c33a27f3a0d4f8562ca46242
ShunKato0625 <shunkato0625@gmail.com>
2022-04-03 10:36:30 +0000

    [Update]dotenv-rails導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.hosts << "c7658fc3c4844d72be52cb720f90c38b.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "91ce9457429a486a87012e1be1790e71.vfs.cloud9.us-east-1.amazonaws.com"
 
 end
